7665cf4dd9fcf56429e56f1e94ee618ff2487118
whz14 <yqwanghz@gmail.com>
2016-11-18 15:33:07 +0800

    git ignore and vhd included

--- pro3/ram_uart/ram_uart.vhd
+++ pro3/ram_uart/ram_uart.vhd
@@ -22,7 +22,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
@@ -53,45 +53,8 @@ entity ram_uart is
 end ram_uart;
 
 architecture Behavioral of ram_uart is
-   component ram
-    port(
-      SW : in STD_LOGIC_VECTOR(15 downto 0);
-      CLK : in STD_LOGIC;
-      RST : in STD_LOGIC;
-      L : out STD_LOGIC_VECTOR(15 downto 0);
-      
-      Ram1Addr : out STD_LOGIC_VECTOR(17 downto 0);
-      Ram1Data : inout STD_LOGIC_VECTOR(15 downto 0);
-      Ram1OE : out STD_LOGIC;
-      Ram1WE : out STD_LOGIC;
-      Ram1EN : out STD_LOGIC;
-      
-      Ram2Addr : out STD_LOGIC_VECTOR(17 downto 0);
-      Ram2Data : inout STD_LOGIC_VECTOR(15 downto 0);
-      Ram2OE : out STD_LOGIC;
-      Ram2WE : out STD_LOGIC;
-      Ram2EN : out STD_LOGIC;
-      
-      rdn : out STD_LOGIC;
-      wrn : out STD_LOGIC
-      );
-  end component;
-  component uart
-    port(
-      clk : in  STD_LOGIC;
-      rst : in  STD_LOGIC;
-      L : out  STD_LOGIC_VECTOR (7 downto 0);
-      Ram1Data : inout  STD_LOGIC_VECTOR (7 downto 0);
-      Ram1OE : out  STD_LOGIC;
-      Ram1WE : out  STD_LOGIC;
-      Ram1EN : out  STD_LOGIC;
-      rdn : out  STD_LOGIC;
-      wrn : out  STD_LOGIC;
-      data_ready : in  STD_LOGIC;
-      tbre : in  STD_LOGIC;
-      tsre : in  STD_LOGIC);
-  end component;
-  signal state: STD_LOGIC;
+   
+  signal main_state: STD_LOGIC;
   signal iL_ram: std_logic_vector(15 downto 0);
   signal iL_uart: std_logic_vector(7 downto 0);
   signal iRam1Data_uart: std_logic_vector(7 downto 0) ;
@@ -106,60 +69,140 @@ architecture Behavioral of ram_uart is
   signal irdn_uart: STD_LOGIC;
   signal iwrn_ram: STD_LOGIC;
   signal iwrn_uart: STD_LOGIC;
+
+  signal addr : STD_LOGIC_VECTOR(17 DOWNTO 0);
+  signal data : STD_LOGIC_VECTOR(15 DOWNTO 0);
+  signal adder16 : STD_LOGIC_VECTOR(15 DOWNTO 0);
+  signal adder18 : STD_LOGIC_VECTOR(17 DOWNTO 0);
 begin
-  uart_cmpt: uart port map(
-    clk => clk_uart,
-    rst => rst,
-    L => iL_uart,
-    Ram1Data => iRam1Data_uart,
-    Ram1OE => iRam1OE_uart,
-    Ram1EN => iRam1EN_uart,
-    Ram1WE => iRam1WE_uart,
-    rdn => irdn_uart,
-    wrn => iwrn_uart,
-    data_ready => data_ready,
-    tbre => tbre,
-    tsre => tsre
-  );
-  ram_cmpt: ram port map(
-    sw => sw,
-    clk => clk_ram,
-    rst => rst,
-    L => iL_ram,
-    
-    Ram1Addr => Ram1Addr,
-    Ram1Data => iRam1Data_ram,
-    Ram1OE => iRam1OE_ram,
-    Ram1WE => iRam1WE_ram,
-    Ram1EN => iRam1EN_ram,
-
-    Ram2Addr => Ram2Addr,
-    Ram2Data => Ram2Data,
-    Ram2OE => Ram2OE,
-    Ram2WE => Ram2WE,
-    Ram2EN => Ram2EN,
-
-    rdn => irdn_ram,
-    wrn => iwrn_ram
-  );
   switch_proc : process(clk_ram, rst)
   begin
     state <= (not(clk_ram or rst) and sw(0)) or ((clk_ram or rst) and state);
   end process ; -- switch_proc
-  main_proc : process(clk_ram, clk_uart, rst, iRam1OE_ram, iRam1WE_ram)
-  begin
-    case state is
-      when '0' => -- ram read-write
-        L <= iL_ram;
-        if iRam1WE_ram = '0' then
-          Ram1Data <= iRam1Data_ram;
-        elsif iRam1OE_ram = '0' then
-          iRam1Data_ram <= Ram1Data;
-        end if;
-      when '1' =>
-        
-      when others =>
-    end case ;
-  end process ; -- main_proc
+  PROCESS(CLK,RST)
+    VARIABLE work_state : INTEGER RANGE 0 TO 6 := 0;
+    -- 0:准备读入地址
+    -- 1:准备读入数据
+    -- 2:开始写
+    -- 3:开始读
+    -- 4:终�
+    --variable visit_state : INTEGER RANGE 0 TO 2 := 0;
+    -- 0:写建立开�
+    -- 1:写入
+    -- 2：写保持开�
+    variable count : INTEGER;
+    
+    BEGIN
+      adder16 <= "0000000000000001";
+      adder18 <= "000000000000000001";
+      IF(RST = '0') THEN  --初始�
+        work_state := 0;
+        count := 0;
+        rdn <= '1';
+        wrn <= '1';
+      ELSIF(CLK' EVENT AND CLK = '0') THEN
+        case work_state is
+          when 0 =>
+            addr <= "00" & SW;
+            work_state := work_state + 1;
+            iL_ram <= "1111000011110000";
+
+          when 1 =>
+            data <= SW;
+            work_state := work_state + 1;
+            iL_ram(7 downto 0) <= addr(7 downto 0);--显示低八�
+            iL_ram(15 downto 8) <= data(7 downto 0);
+            Ram1OE <= '1';--�
+            Ram1WE <= '1';
+            Ram1En <= '0';
+
+          when 2 =>
+            if(count < 10) then--计数器没�0
+              addr <= addr + adder18;
+              data <= data + adder16;
+              iRam1Addr_ram <= addr;
+              iRam1Data_ram <= data;
+              Ram1OE <= '1';--�
+              Ram1WE <= '0';
+              iL_ram(7 downto 0) <= addr(7 downto 0);--显示低八�
+              iL_ram(15 downto 8) <= data(7 downto 0);
+              count := count + 1;
+            else
+              addr <= addr - "000000000000001010";--恢复addr
+              iRam1Addr_ram <= addr - "000000000000001010";
+              iRam1Data_ram <= "ZZZZZZZZZZZZZZZZ";--写入数据高阻�
+              Ram1OE <= '0';--�
+              Ram1WE <= '1';
+              iL_ram <= "1111111111111111";
+              count := 0;--重置计数�
+              work_state := 3;--进入读状�
+            end if;
+          
+          when 3 =>
+            if(count < 10) then--计数器没�0      
+              iRam1Data_ram <= "ZZZZZZZZZZZZZZZZ";--写入数据高阻�
+              iRam1Addr_ram <= addr + adder18;--准备下次写的ram1addr
+              addr <= addr + adder18;--自增
+              Ram1OE <= '0';--�
+              Ram1WE <= '1';
+              iL_ram <= iRam1Data_ram;
+              count := count + 1;
+            else
+              addr <= addr - "000000000000001010";--恢复addr
+              data <= data - "0000000000001011";--恢复data
+              Ram2Addr <= addr - "000000000000001010";--设置ram2addr
+              Ram2Data <= data - "0000000000001011";--设置ram2data，此处需要减1
+              Ram2OE <= '1';--�
+              Ram2WE <= '1';
+              Ram2En <= '0';
+              iL_ram <= "1111111111111111";
+              count := 0;--重置计数�
+              work_state := 4;--进入预备写ram2     
+            end if;
+            
+          when 4 =>
+            if(count < 10) then--计数器没�0
+              addr <= addr + adder18;
+              data <= data + adder16;
+              Ram2Addr <= addr;
+              Ram2Data <= data;
+              Ram2OE <= '1';--�
+              Ram2WE <= '0';
+              iL_ram(7 downto 0) <= addr(7 downto 0);--显示低八�
+              iL_ram(15 downto 8) <= data(7 downto 0);
+              count := count + 1;
+            else
+              addr <= addr - "000000000000001010";--恢复addr
+              Ram2Addr <= addr - "000000000000001010";
+              Ram2Data <= "ZZZZZZZZZZZZZZZZ";--写入数据高阻�
+              Ram2OE <= '0';--�
+              Ram2WE <= '1';
+              iL_ram <= "1111111111111111";
+              count := 0;--重置计数�
+              work_state := 5;--进入读状�
+            end if;
+            
+          when 5 =>
+            if(count < 10) then--计数器没�0      
+              Ram2Data <= "ZZZZZZZZZZZZZZZZ";--写入数据高阻�
+              Ram2Addr <= addr + adder18;--准备下次写的ram1addr
+              addr <= addr + adder18;--自增
+              Ram2OE <= '0';--�
+              Ram2WE <= '1';
+              iL_ram <= Ram2Data;
+              count := count + 1;
+            else
+              addr <= addr - "000000000000001010";--恢复addr
+              data <= data - "0000000000001010";--恢复data
+              iL_ram <= "1111111111111111";
+              count := 0;--重置计数�
+              work_state := 6;--进入终�     
+            end if;
+          
+          when 6 =>
+        end case;
+        --end if;
+      END IF;
+    END PROCESS;
 
 end Behavioral;
