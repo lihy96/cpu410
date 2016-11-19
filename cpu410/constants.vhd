80f6d4373d4b39f49c294523baaca5bd6998b546
lihy96 <lhy237451594@vip.qq.com>
2016-11-20 01:26:35 +0800

    fix bug

--- cpu410/constants.vhd
+++ cpu410/constants.vhd
@@ -22,8 +22,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 
 package constants is
-	constant Pc_reset: std_logic := 1;	--pc是否reset的控制信号
-	constant Pc_pause: std_logic := 1;	--pc是否pause的控制信号
+	constant Pc_reset: std_logic := '1';	--pc是否reset的控制信号
+	constant Pc_pause: std_logic := '1';	--pc是否pause的控制信号
 	constant Pc_origin_address : std_logic_vector(15 downto 0) := "0000000000000000";	--pc的初始地址
 	constant Pc_offset: std_logic_vector(15 downto 0) := "0000000000000001";	--每次pc的偏移量
 	type RegArray is array(11 down to 0) of std_logic_vector(15 downto 0);	-- 11个寄存器？8个通用？？？？？？？
