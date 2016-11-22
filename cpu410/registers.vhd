387abaa73754775367e2cededa53d96527207204
lihy96 <lhy237451594@vip.qq.com>
2016-11-22 11:36:52 +0800

    change mux between pc and instruction ram

--- cpu410/registers.vhd
+++ cpu410/registers.vhd
@@ -9,7 +9,6 @@ entity registers is
 port (
 	clk, rst: in std_logic;
 	r1_addr, r2_addr: in std_logic_vector(3 downto 0);
-	r1_read_en, r2_read_en: in std_logic;
 	r1_data, r2_data: out std_logic_vector(15 downto 0);
 	write_addr: in std_logic_vector(3 downto 0);
 	write_en: in std_logic;
@@ -19,6 +18,7 @@ end entity;
 
 architecture bhv of registers is
 signal reg_array: RegArray := (others => ZeroWord);
+signal r1_read_en, r2_read_en: in std_logic := ReadEnable;
 begin
 
 	process (clk)
