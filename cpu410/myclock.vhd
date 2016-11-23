8c94a40e20a915eb98c7d9dcd7e559fc29052786
lihy96 <lhy237451594@vip.qq.com>
2016-11-23 17:42:28 +0800

    with some bug

--- cpu410/myclock.vhd
+++ cpu410/myclock.vhd
@@ -1,82 +1,47 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    23:58:32 11/30/2014 
--- Design Name: 
--- Module Name:    Clock - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use work.constants.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity myclock is
-    Port ( 
-				Reset: in std_logic;
-				Clock_50 : in  STD_LOGIC;
-				Clock_50_out : out  STD_LOGIC;
-				Clock_25_out : out  STD_LOGIC;
-				Clock_12_out : out  STD_LOGIC);
+	port(
+		Reset : IN  std_logic;
+		Clock_50: 		in std_logic;
+		Clock_50_out, Clock_25_out, Clock_12_out: out std_logic	--输出时钟频率
+	);
 end myclock;
 
 architecture Behavioral of myclock is
-	shared variable count: integer := 0;
-	shared variable count1: integer := 0;
-	shared variable count2: integer := 0;
-	
-	signal Clock_25_tmp: std_logic;
-	signal Clock_12_tmp: std_logic;
-	
+	signal clk_12M: std_logic := '0';
+	signal clk_25M:	std_logic := '0';
+	shared variable count_flash: integer := 0;	--等待flash读取
+	shared variable count_25: integer := 0;	--分别用于2分频，4分频，很慢的频率调试用
+	shared variable count_12: integer := 0;
 begin
 	Clock_50_out <= Clock_50;
-	Clock_25_out <= Clock_25_tmp;
-	Clock_12_out <= Clock_12_tmp;
-	
-	process (Clock_50, Reset)
+	Clock_25_out <= clk_25M;
+	Clock_12_out <= clk_12M;
+	process(Clock_50)
 	begin
-		if (Reset = '0') then
-			count1 := 0;
-			count2 := 0;
-			Clock_25_tmp <= '0';
-			Clock_12_tmp <= '0';
-		elsif falling_edge(Clock_50) then
-			if (count > Clock_wait_num_before_work) then	-- you can use this to do something
-				
-				count1 := count1 + 1;
-				
-				if (count1 = 1) then
-					Clock_25_tmp <= not Clock_25_tmp;
-					count1 := 0;
+		if Clock_50'event and Clock_50 = '0' then
+			if (count_flash > Clock_wait_num_before_work) then	-- 程序刚启动先不执行，等待读取flash
+				count_25 := count_25 + 1;
+				if (count_25 = 1) then	--这里应该等于1，25M
+					clk_25M <= not clk_25M;
+					count_25 := 0;
 				end if;
 				
-				count2 := count2 + 1;
-				if (count2 = 8) then
-					Clock_12_tmp <= not Clock_12_tmp;
-					count2 := 0;
+				count_12 := count_12 + 1;
+				if (count_12 = 3) then	--4分频，12M
+					clk_12M <= not clk_12M;
+					count_12 := 0;
 				end if;
 			else
-				count := count + 1;
+				count_flash := count_flash + 1;
 			end if;
 		end if;
+		Clock_12_out <= '0';
 	end process;
 
 end Behavioral;
