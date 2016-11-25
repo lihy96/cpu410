19ce2cf23bb73b5812dcd0037a7f3d4ce857bf7b
whz14 <yqwanghz@gmail.com>
2016-11-26 00:09:46 +0800

    reg0 not-wanted forward bug fixed

--- cpu410/forward.vhd
+++ cpu410/forward.vhd
@@ -21,18 +21,22 @@ architecture Behavioral of forward is
 	signal reg2: std_logic_vector(3 downto 0) ;
 	signal reg0: std_logic_vector(3 downto 0) ;
 	signal reg1_prev_type: std_logic_vector(1 downto 0) ;
-	signal reg2_prev_type: std_logic_vector(1 downto 0) ;
+	--signal reg2_prev_type: std_logic_vector(1 downto 0) ;
 	signal nxt_reg1: std_logic_vector(1 downto 0) ;
 	signal nxt_reg2: std_logic_vector(1 downto 0) ;
 begin
 	reg1 <= reg_bus(11 downto 8);
 	reg2 <= reg_bus(7 downto 4);
 	reg0 <= reg_bus(3 downto 0);
-	process(clk, reg0, reg1, reg2, reg_bus, reg_prev, reg_prev1, reg_prev2, reg_prev_type, reg1_prev_type, reg2_prev_type)
+	process(clk, reg0, reg1, reg2, reg_bus, reg_prev, reg_prev1, reg_prev2, reg_prev_type, reg1_prev_type)
 	begin
 		if reg0 = reg_prev1 and reg1_prev_type = WB_EXE then
 			reg_choose0 <= REG_CHOOSE1_EXEFWD;
-		elsif reg0 = reg_prev2 then
+		elsif reg0 = reg_prev2 and reg0 /= IMG_REG then
+		-- only choose the forward value when the refered reg is not IMG one
+		-- it is a must since though the reg0 is IMG, `reg1` may not.
+		-- In this way, reg1 might be choosen to a wrong value due to IMG_REG = IMG_REG
+		-- no need in others since even a wrong reg1 or reg2 are useless.
 			reg_choose0 <= REG_CHOOSE1_MEMFWD;
 		else
 			reg_choose0 <= REG_CHOOSE1_RF;
@@ -55,7 +59,7 @@ begin
 		end if;
 		if rising_edge(clk) then
 			reg1_prev_type <= reg_prev_type;
-			reg2_prev_type <= reg1_prev_type;
+			--reg2_prev_type <= reg1_prev_type;
 			reg_choose1 <= nxt_reg1;
 			reg_choose2 <= nxt_reg2;
 		end if;
